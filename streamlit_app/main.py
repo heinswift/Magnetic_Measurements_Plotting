76010f20a9974147a3a2a6d23d89390a7d6d2321
Hein Schnell <hein-erik.schnell@t-online.de>
2022-01-30 19:33:23 +0100

    Update main.py

--- streamlit_app/main.py
+++ streamlit_app/main.py
@@ -1,5 +1,25 @@
 import streamlit as st
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
 
 st.title("Here comes the title")
 st.write("Here comes some text.")
 st.button("Click Here!")
+
+def f(x, y):
+    return np.sin(x) ** 10 + np.cos(10 + y * x) * np.cos(x)
+
+x = np.linspace(0, 5, 900)
+y = np.linspace(0, 5, 900)
+
+X, Y = np.meshgrid(x, y)
+Z = f(X, Y)
+
+n_color_steps = st.number_input("Anzahl Farbstufen", min_value=0, max_value=100,value=10,step=1)
+n_color_steps = int(n_color_steps)
+fig, ax  = plt.subplots()
+plt.contourf(X, Y, Z, n_color_steps-1, cmap='RdGy')
+plt.colorbar()
+
+st.pyplot(fig)
